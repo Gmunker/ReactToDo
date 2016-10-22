0dfd9a0b8e054eff543edf91d6fc56090c080259
Greg Munker <gmunker@munkerdesigns.com>
2016-10-22 17:37:28 -0400

    Add auth to action tests

--- app/tests/actions/actions.test.jsx
+++ app/tests/actions/actions.test.jsx
@@ -43,18 +43,6 @@ describe('Actions', () => {
 		expect(res).toEqual(action);
 	});
 
-	it('should create todo and dispatch addTodo', (done) => {
-		const store = createMockStore({});
-		const todoText = 'My todo item';
-
-		store.dispatch(actions.startAddTodo(todoText)).then(() => {
-			const actions = store.getActions();
-			expect(actions[0]).toInclude({type: 'ADD_TODO'});
-			expect(actions[0].todo).toInclude({text: todoText});
-			done();
-		}).catch(done);
-	});
-
 	it('should generate add todos action object', () => {
 		let todos = [
 			{
@@ -89,24 +77,33 @@ describe('Actions', () => {
 
 	describe('Tests with firebade todos', () => {
 		let testTodoRef;
+		let uid;
+		let todosRef;
 
 		beforeEach((done) => {
-			let todosRef = firebaseRef.child('todos');
+			firebase.auth().signInAnonymously().then((user) => {
+				uid = user.uid;
+				todosRef = firebaseRef.child(`users/${uid}/todos`);
 
-			todosRef.remove().then(() => {
-				testTodoRef = firebaseRef.child('todos').push();
-				return testTodoRef.set({text: 'something to do', completed: false, createdAt: 123})
+				return todosRef.remove();
+			}).then(() => {
+				testTodoRef = todosRef.push();
+				return testTodoRef.set({
+					text: 'something to do',
+					completed: false,
+					createdAt: 123
+				});
 			})
 			.then(() => done())
 			.catch(done);
 		});
 
 		afterEach((done) => {
-			testTodoRef.remove().then(() => done());
+			todosRef.remove().then(() => done());
 		});
 
 		it('should toggleTodo and dispatch UPDATE_TODO action', (done) => {
-			const store = createMockStore({});
+			const store = createMockStore({auth: {uid}});
 			const action = actions.startToggleTodo(testTodoRef.key, true);
 
 			store.dispatch(action).then(() => {
@@ -123,7 +120,7 @@ describe('Actions', () => {
 		}); // it should toggleTodo..
 
 		it('should add todo to todos and dispatch ADD_TODOS', (done) => {
-			const store = createMockStore({});
+			const store = createMockStore({auth: {uid}});
 			const action = actions.startAddTodos();
 
 			store.dispatch(action).then(() => {
@@ -135,6 +132,18 @@ describe('Actions', () => {
 				done();
 			},done);
 		})// should add 1 todo to todos
+
+		it('should create todo and dispatch addTodo', (done) => {
+			const store = createMockStore({auth: {uid}});
+			const todoText = 'My todo item';
+
+			store.dispatch(actions.startAddTodo(todoText)).then(() => {
+				const actions = store.getActions();
+				expect(actions[0]).toInclude({type: 'ADD_TODO'});
+				expect(actions[0].todo).toInclude({text: todoText});
+				done();
+			}).catch(done);
+		});
 	});// Tests with firebade todos
 
 	describe('Tests with Auth', () => {

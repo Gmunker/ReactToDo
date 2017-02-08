26566c19a38381f71623122d8f452942a9399f3f
Greg Munker <gmunker@gmail.com>
2017-02-08 10:41:10 -0700

    Added personal stylings

--- app/components/Login.jsx
+++ app/components/Login.jsx
@@ -13,10 +13,31 @@ export class Login extends React.Component {
   }
   render() {
     return (
-      <div>
-        <h1 className="page-title">Todo App</h1>
+
+     <div>
+
+     <div className="top-bar columns small-12 medium-12 large-8 small-centered">
+       <h1 className="fcc-title">React Todo App</h1>
+
+
+        <div className="credits">
+          <h2>Designed and Created by: </h2>
+          <h2>Greg Munker</h2>
+          <h2><a className="github-link" href="https://github.com/Gmunker/ReactToDo">(GitHub Repo)</a></h2>
+          <div className="social-buttons expanded button-group">
+              <a className="button secondary" href="http://www.facebook.com/gmunker" target="_blank">Facebook</a>
+              <a className="button secondary" href="https://plus.google.com/u/0/+GregMunker" target="_blank">Google+</a>
+              <a className="button secondary" href="https://github.com/Gmunker" target="_blank">Github</a>
+              <a className="button secondary" href="https://www.linkedin.com/profile/preview?locale=en_US&trk=prof-0-sb-preview-primary-button" target="_blank">LinkedIn</a>
+              <a className="button secondary" href="https://twitter.com/Gmunker" target="_blank">Twitter</a>
+          </div>
+        </div>
+        </div>
+
+
+        
         <div className="row">
-          <div className="columns small-centered small-10 medium-6 large-4">
+          <div className="login-container columns small-centered small-10 medium-6 large-4">
             <div className="callout callout-auth">
               <h3>Login</h3>
               <p>Login with GitHub account below</p>
@@ -25,6 +46,7 @@ export class Login extends React.Component {
           </div>
         </div>
       </div>
+
 
     )
   }
--- app/components/TodoApp.jsx
+++ app/components/TodoApp.jsx
@@ -16,12 +16,36 @@ export class TodoApp extends React.Component {
 	render() {
 		return (
 			<div>
+
+			<div className="columns small-12 medium-12 large-8 small-centered">
+			<div className="top-bar">
+       <h1 className="fcc-title">React Todo App</h1>
+
+
+        <div className="credits">
+          <h2>Designed and Created by: </h2>
+          <h2>Greg Munker</h2>
+          <h2><a className="github-link" href="https://github.com/Gmunker/ReactToDo">(GitHub Repo)</a></h2>
+          <div className="social-buttons expanded button-group">
+              <a className="button secondary" href="http://www.facebook.com/gmunker" target="_blank">Facebook</a>
+              <a className="button secondary" href="https://plus.google.com/u/0/+GregMunker" target="_blank">Google+</a>
+              <a className="button secondary" href="https://github.com/Gmunker" target="_blank">Github</a>
+              <a className="button secondary" href="https://www.linkedin.com/profile/preview?locale=en_US&trk=prof-0-sb-preview-primary-button" target="_blank">LinkedIn</a>
+              <a className="button secondary" href="https://twitter.com/Gmunker" target="_blank">Twitter</a>
+          </div>
+        </div>
+				
+
+        </div>
+ {/*Below the top bar container*/}
 				<div className="page-actions">
 					<a href="#" onClick={this.onLogout.bind(this)}>Logout</a>
 				</div>
-				<h1 className="page-title">Todo App</h1>
+				</div>
+
+				
 				<div className="row">
-					<div className="column small-centered small-11 medium-6 large-5">
+					<div className="column small-centered small-11 medium-10 large-5">
 						<div className="container">
 							<Search/>
 							<TodoList/>
